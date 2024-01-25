d263a36a348998c0461b7c2b6c06bc4300a10be9
Scriptbash <98601298+Scriptbash@users.noreply.github.com>
2024-01-25 17:36:00 -0500

    Add some settings

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../theme_provider.dart';
 import './institutions_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -13,7 +15,13 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  //final FocusNode _buttonFocusNode = FocusNode(debugLabel: 'Menu Button');
+  late Future<List<String>> appInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    appInfo = getAppVersion();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +34,64 @@ class _SettingsScreenState extends State<SettingsScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: ListTile(
+                onTap: () {},
+                title: Column(
+                  children: [
+                    Image.asset(
+                      'assets/icon/icon.png',
+                      width: 50,
+                    ),
+                    Text('Wispar'),
+                  ],
+                ),
+                subtitle: Column(children: [
+                  FutureBuilder<List<String>>(
+                    future: appInfo,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        List<String>? appInfo = snapshot.data;
+
+                        if (appInfo != null && appInfo.length == 2) {
+                          return Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Version ${appInfo[0]}'),
+                              Text(' build ${appInfo[1]}'),
+                            ],
+                          );
+                        }
+                      }
+                      return CircularProgressIndicator();
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton.icon(
+                          onPressed: () {
+                            launchUrl(
+                                Uri.parse(
+                                    'https://github.com/Scriptbash/Wispar'),
+                                mode: LaunchMode.platformDefault);
+                          },
+                          icon: Icon(Icons.code),
+                          label: Text('Source code')),
+                      TextButton.icon(
+                          onPressed: () {
+                            launchUrl(
+                                Uri.parse(
+                                    'https://github.com/Scriptbash/Wispar/issues'),
+                                mode: LaunchMode.platformDefault);
+                          },
+                          icon: Icon(Icons.bug_report_outlined),
+                          label: Text('Report an issue')),
+                    ],
+                  )
+                ]),
+              ),
+            ),
             ListTile(
                 onTap: () {
                   _showThemeDialog(context);
@@ -41,6 +107,34 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   SizedBox(width: 32),
                   Text(_getThemeSubtitle(
                       context, Provider.of<ThemeProvider>(context).themeMode)),
+                ])),
+            ListTile(
+                onTap: () {
+                  _showUnpaywallDialog(context);
+                },
+                title: Row(
+                  children: [
+                    Icon(Icons.lock_open_outlined),
+                    SizedBox(width: 8),
+                    Text('Unpaywall'),
+                  ],
+                ),
+                subtitle: Row(children: [
+                  SizedBox(width: 32),
+                  FutureBuilder<String?>(
+                    future: getUnpaywallStatus(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Center(
+                          child: Text(snapshot.data ?? 'Enabled'),
+                        );
+                      } else {
+                        return Center(
+                          child: Text('Enabled'),
+                        );
+                      }
+                    },
+                  ),
                 ])),
             ListTile(
               onTap: () async {
@@ -95,6 +189,30 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 ),
               ]),
             ),
+            ListTile(
+              onTap: () {},
+              title: Row(
+                children: [
+                  Icon(Icons.dns_outlined),
+                  SizedBox(width: 8),
+                  Text('Database'),
+                ],
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                // Will need to point it to the privacy policy once it's created
+                launchUrl(Uri.parse('https://github.com/Scriptbash/Wispar/'),
+                    mode: LaunchMode.platformDefault);
+              },
+              title: Row(
+                children: [
+                  Icon(Icons.privacy_tip_outlined),
+                  SizedBox(width: 8),
+                  Text('Privacy policy'),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -118,6 +236,62 @@ class _SettingsScreenState extends State<SettingsScreen> {
     prefs.remove('institution_name');
     prefs.remove('institution_url');
     setState(() {});
+  }
+
+  Future<void> saveUnpaywallPreference(String status) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('unpaywall', status);
+  }
+
+  Future<String?> getUnpaywallStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {});
+    return prefs.getString('unpaywall') ?? 'Enabled';
+  }
+
+  void _showUnpaywallDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return FutureBuilder<String?>(
+          future: getUnpaywallStatus(),
+          builder: (context, snapshot) {
+            String? currentStatus = snapshot.data;
+            currentStatus ??= 'Enabled';
+            return AlertDialog(
+              title: Text('Unpaywall'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  RadioListTile<String>(
+                    title: Text('Enabled'),
+                    value: 'Enabled',
+                    groupValue: currentStatus,
+                    onChanged: (value) {
+                      if (value != null) {
+                        saveUnpaywallPreference(value);
+                      }
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  RadioListTile<String>(
+                    title: Text('Disabled'),
+                    value: 'Disabled',
+                    groupValue: currentStatus,
+                    onChanged: (value) {
+                      if (value != null) {
+                        saveUnpaywallPreference(value);
+                      }
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 }
 
@@ -184,4 +358,9 @@ String _getThemeSubtitle(BuildContext context, ThemeMode? themeMode) {
     default:
       return 'Unknown';
   }
+}
+
+Future<List<String>> getAppVersion() async {
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  return [packageInfo.version, packageInfo.buildNumber];
 }
