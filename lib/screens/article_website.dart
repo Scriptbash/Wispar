41be5ca352cccffd70b878400bfa40fb8fb664c0
Scriptbash <98601298+Scriptbash@users.noreply.github.com>
2025-01-02 13:23:41 -0500

    Use Unpaywall preferences

--- lib/screens/article_website.dart
+++ lib/screens/article_website.dart
@@ -25,16 +25,13 @@ class _ArticleWebsiteState extends State<ArticleWebsite> {
   final GlobalKey webViewKey = GlobalKey();
   InAppWebViewController? webViewController;
   InAppWebViewSettings settings = InAppWebViewSettings(
-      isInspectable: kDebugMode,
-      mediaPlaybackRequiresUserGesture: false,
-      allowsInlineMediaPlayback: true,
-      javaScriptEnabled: true,
-      javaScriptCanOpenWindowsAutomatically: true,
-      useOnDownloadStart: true,
-      //iframeAllow: "camera; microphone",
-      userAgent:
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0',
-      iframeAllowFullscreen: true);
+    isInspectable: kDebugMode,
+    mediaPlaybackRequiresUserGesture: true,
+    javaScriptEnabled: true,
+    javaScriptCanOpenWindowsAutomatically: true,
+    useOnDownloadStart: true,
+    iframeAllowFullscreen: true,
+  );
 
   PullToRefreshController? pullToRefreshController;
   String url = "";
@@ -42,6 +39,7 @@ class _ArticleWebsiteState extends State<ArticleWebsite> {
   final urlController = TextEditingController();
   late String pdfUrl = '';
   late String proxyUrl = '';
+  bool isReadyToLoad = false;
 
   @override
   void initState() {
@@ -66,26 +64,36 @@ class _ArticleWebsiteState extends State<ArticleWebsite> {
   }
 
   Future<void> checkUnpaywallAvailability() async {
-    final Unpaywall result =
-        await UnpaywallService.checkAvailability(widget.publicationCard.doi);
-    if (result.pdfUrl.isNotEmpty) {
-      setState(() {
-        pdfUrl = result.pdfUrl;
-      });
-    } else {
-      await loadProxyUrl();
+    final prefs = await SharedPreferences.getInstance();
+    String unpaywallPrefs = prefs.getString('unpaywall') ?? 'Enabled';
+
+    // Check Unpaywall for PDF if enabled in the settings
+    if (unpaywallPrefs == 'Enabled') {
+      final Unpaywall result =
+          await UnpaywallService.checkAvailability(widget.publicationCard.doi);
+      if (result.pdfUrl.isNotEmpty) {
+        setState(() {
+          pdfUrl = result.pdfUrl;
+        });
+        _showSnackBar(AppLocalizations.of(context)!.unpaywallarticle);
+      }
     }
+    // If no PDF from Unpaywall or Unpaywall is disabled, check the proxy
     if (pdfUrl.isEmpty) {
+      await loadProxyUrl();
       if (proxyUrl.isNotEmpty) {
+        setState(() {
+          pdfUrl = proxyUrl + widget.publicationCard.url;
+        });
         _showSnackBar(AppLocalizations.of(context)!.forwardedproxy);
-        pdfUrl = proxyUrl + widget.publicationCard.url;
       } else {
         pdfUrl = widget.publicationCard.url;
       }
-    } else {
-      _showSnackBar(AppLocalizations.of(context)!.unpaywallarticle);
     }
-    setupController();
+    setState(() {
+      isReadyToLoad =
+          pdfUrl.isNotEmpty; // Ensure WebView only loads when URL is ready
+    });
   }
 
   Future<void> loadProxyUrl() async {
@@ -94,12 +102,7 @@ class _ArticleWebsiteState extends State<ArticleWebsite> {
       proxyUrl = prefs.getString('institution_url') ?? '';
       proxyUrl = proxyUrl.replaceAll('\$@', '');
     });
-  }
-
-  void setupController() {
-    if (pdfUrl.isNotEmpty) {
-      webViewController?.loadUrl(urlRequest: URLRequest(url: WebUri(pdfUrl)));
-    }
+    print('loaded proxy url is $proxyUrl');
   }
 
   void _showSnackBar(String message) {
@@ -120,112 +123,131 @@ class _ArticleWebsiteState extends State<ArticleWebsite> {
           Expanded(
             child: Stack(
               children: [
-                InAppWebView(
-                  key: webViewKey,
-                  initialUrlRequest: URLRequest(url: WebUri(pdfUrl)),
-                  initialSettings: settings,
-                  pullToRefreshController: pullToRefreshController,
-                  onWebViewCreated: (controller) {
-                    webViewController = controller;
-                  },
-                  onLoadStart: (controller, url) {
-                    setState(() {
-                      this.url = url.toString();
-                      urlController.text = this.url;
-                    });
-                  },
-                  onPermissionRequest: (controller, request) async {
-                    return PermissionResponse(
-                        resources: request.resources,
-                        action: PermissionResponseAction.GRANT);
-                  },
-                  shouldOverrideUrlLoading:
-                      (controller, navigationAction) async {
-                    //var uri = navigationAction.request.url!;
-
-                    return NavigationActionPolicy.ALLOW;
-                  },
-                  onReceivedServerTrustAuthRequest:
-                      (controller, challenge) async {
-                    //print("challenge $challenge");
-                    return ServerTrustAuthResponse(
-                        action: ServerTrustAuthResponseAction.PROCEED);
-                  },
-                  onDownloadStartRequest: (controller, url) async {
-                    if (url.mimeType == 'application/pdf') {
-                      final response = await http.get(url.url);
-                      if (response.body.startsWith('%PDF')) {
-                        // Needed due to shenanigans of Elsevier and the likes
-                        final appDir = await getApplicationDocumentsDirectory();
-                        final pdfFile =
-                            File('${appDir.path}/${url.suggestedFilename}');
-                        await pdfFile.writeAsBytes(response.bodyBytes);
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => PdfReader(
-                            pdfUrl: pdfFile.path,
-                            publicationCard: widget.publicationCard,
-                          ),
-                        ));
-                      } else {
-                        launchUrl(Uri.parse(pdfUrl));
-                      }
-                    } else {
-                      debugPrint('The file is not a PDF. Not downloading.');
-                    }
-                  },
-                  onLoadStop: (controller, url) async {
-                    pullToRefreshController?.endRefreshing();
-                    setState(() {
-                      this.url = url.toString();
-                      urlController.text = this.url;
-                    });
-                    controller
-                        .evaluateJavascript(source: "document.title;")
-                        .then((result) {
-                      final title = result.trim();
-
-                      /* Check if the title is "Host Needed"    
+                // Only show the WebView once the URL is ready
+                isReadyToLoad
+                    ? InAppWebView(
+                        key: webViewKey,
+                        initialUrlRequest: URLRequest(url: WebUri(pdfUrl)),
+                        initialSettings: settings,
+                        pullToRefreshController: pullToRefreshController,
+                        onWebViewCreated: (controller) {
+                          webViewController = controller;
+                        },
+                        onLoadStart: (controller, url) {
+                          setState(() {
+                            this.url = url.toString();
+                            urlController.text = this.url;
+                          });
+                        },
+                        onPermissionRequest: (controller, request) async {
+                          return PermissionResponse(
+                              resources: request.resources,
+                              action: PermissionResponseAction.GRANT);
+                        },
+                        shouldOverrideUrlLoading:
+                            (controller, navigationAction) async {
+                          var uri = navigationAction.request.url!;
+                          if (![
+                            "http",
+                            "https",
+                            "file",
+                            "chrome",
+                            "data",
+                            "javascript",
+                            "about"
+                          ].contains(uri.scheme)) {
+                            if (await canLaunchUrl(uri)) {
+                              // Launch the App
+                              await launchUrl(uri);
+                              // and cancel the request
+                              return NavigationActionPolicy.CANCEL;
+                            }
+                          }
+                          return NavigationActionPolicy.ALLOW;
+                        },
+                        onReceivedServerTrustAuthRequest:
+                            (controller, challenge) async {
+                          return ServerTrustAuthResponse(
+                              action: ServerTrustAuthResponseAction.PROCEED);
+                        },
+                        onDownloadStartRequest: (controller, url) async {
+                          if (url.mimeType == 'application/pdf') {
+                            final response = await http.get(url.url);
+                            if (response.body.startsWith('%PDF')) {
+                              // Needed due to shenanigans of Elsevier and the likes
+                              final appDir =
+                                  await getApplicationDocumentsDirectory();
+                              final pdfFile = File(
+                                  '${appDir.path}/${url.suggestedFilename}');
+                              await pdfFile.writeAsBytes(response.bodyBytes);
+                              Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => PdfReader(
+                                  pdfUrl: pdfFile.path,
+                                  publicationCard: widget.publicationCard,
+                                ),
+                              ));
+                            } else {
+                              launchUrl(Uri.parse(pdfUrl));
+                            }
+                          } else {
+                            debugPrint(
+                                'The file is not a PDF. Not downloading.');
+                          }
+                        },
+                        onLoadStop: (controller, url) async {
+                          pullToRefreshController?.endRefreshing();
+                          setState(() {
+                            this.url = url.toString();
+                            urlController.text = this.url;
+                          });
+                          controller
+                              .evaluateJavascript(source: "document.title;")
+                              .then((result) {
+                            final title = result.trim();
+                            /* Check if the title is "Host Needed"    
                       This will need to be improved as it will probably fail to
                        redirect if the institution has changed the error 
                        landing page or if the page is in a different language.*/
-
-                      if (title == "Host Needed") {
-                        // Load the page without the proxy URL
-                        controller.loadUrl(
-                          urlRequest: URLRequest(
-                              url: WebUri(widget.publicationCard.url)),
-                        );
-                      }
-                    }).catchError((error) {
-                      debugPrint(
-                          'Error occurred while evaluating JavaScript: $error');
-                    });
-                  },
-                  onReceivedError: (controller, request, error) {
-                    pullToRefreshController?.endRefreshing();
-                  },
-                  onProgressChanged: (controller, progress) {
-                    if (progress == 100) {
-                      pullToRefreshController?.endRefreshing();
-                    }
-                    setState(() {
-                      this.progress = progress / 100;
-                      urlController.text = url;
-                    });
-                  },
-                  onUpdateVisitedHistory:
-                      (controller, url, androidIsReload) async {
-                    setState(() {
-                      this.url = url.toString();
-                      urlController.text = this.url;
-                    });
-                  },
-                  onConsoleMessage: (controller, consoleMessage) {
-                    if (kDebugMode) {
-                      print(consoleMessage);
-                    }
-                  },
-                ),
+                            if (title == "Host Needed") {
+                              // Load the page without the proxy URL
+                              controller.loadUrl(
+                                urlRequest: URLRequest(
+                                    url: WebUri(widget.publicationCard.url)),
+                              );
+                            }
+                          }).catchError((error) {
+                            debugPrint(
+                                'Error occurred while evaluating JavaScript: $error');
+                          });
+                        },
+                        onReceivedError: (controller, request, error) {
+                          pullToRefreshController?.endRefreshing();
+                        },
+                        onProgressChanged: (controller, progress) {
+                          if (progress == 100) {
+                            pullToRefreshController?.endRefreshing();
+                          }
+                          setState(() {
+                            this.progress = progress / 100;
+                            urlController.text = url;
+                          });
+                        },
+                        onUpdateVisitedHistory:
+                            (controller, url, androidIsReload) async {
+                          setState(() {
+                            this.url = url.toString();
+                            urlController.text = this.url;
+                          });
+                        },
+                        onConsoleMessage: (controller, consoleMessage) {
+                          if (kDebugMode) {
+                            print(consoleMessage);
+                          }
+                        },
+                      )
+                    : Center(
+                        child:
+                            CircularProgressIndicator()), // Show progress while loading
                 progress < 1.0
                     ? LinearProgressIndicator(value: progress)
                     : Container(),
