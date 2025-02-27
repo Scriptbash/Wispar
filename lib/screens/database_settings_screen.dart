792c228ef7424dfcf601ab669a5eb28c80b8f2cb
Francis Lapointe <98601298+Scriptbash@users.noreply.github.com>
2025-02-27 08:23:03 -0500

    Export and import database (#71)
    * Export database using share_plus
    
    * Use file_picker to export and import DB
    
    * Add divider
    
    * Include downloaded pdf files

--- lib/screens/database_settings_screen.dart
+++ lib/screens/database_settings_screen.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:archive/archive.dart';
 
 class DatabaseSettingsScreen extends StatefulWidget {
   const DatabaseSettingsScreen({Key? key}) : super(key: key);
@@ -37,12 +43,99 @@ class _DatabaseSettingsScreenState extends State<DatabaseSettingsScreen> {
   Future<void> _saveSettings() async {
     if (_formKey.currentState?.validate() ?? false) {
       SharedPreferences prefs = await SharedPreferences.getInstance();
-
       await prefs.setInt('cleanupInterval', _cleanupInterval);
-      await prefs.setInt('fetchInterval', _fetchInterval); // Save as integer
+      await prefs.setInt('fetchInterval', _fetchInterval);
 
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text(AppLocalizations.of(context)!.settingsSaved)),
+      );
+    }
+  }
+
+  Future<void> _exportDatabase() async {
+    try {
+      final appDir = await getApplicationDocumentsDirectory();
+      final databasePath = await getDatabasesPath();
+      String dbPath = '$databasePath/wispar.db';
+      File dbFile = File(dbPath);
+
+      final archive = Archive();
+
+      if (!await dbFile.exists()) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text(AppLocalizations.of(context)!.databaseNotFound)),
+        );
+        return;
+      }
+
+      archive.addFile(ArchiveFile(
+          'wispar.db', dbFile.lengthSync(), await dbFile.readAsBytes()));
+
+      for (var file in appDir.listSync()) {
+        if (file is File && file.path.endsWith('.pdf')) {
+          archive.addFile(ArchiveFile(file.uri.pathSegments.last,
+              file.lengthSync(), await file.readAsBytes()));
+        }
+      }
+
+      final zipData = ZipEncoder().encode(archive);
+
+      String? outputFile = await FilePicker.platform.saveFile(
+        dialogTitle: AppLocalizations.of(context)!.selectDBExportLocation,
+        fileName: 'wispar_backup.zip',
+        bytes: Uint8List.fromList(zipData),
+      );
+
+      if (outputFile == null) return;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(AppLocalizations.of(context)!.databaseExported)),
+      );
+    } catch (e) {
+      debugPrint("Database export error: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(
+                "${AppLocalizations.of(context)!.databaseExportFailed}: $e")),
+      );
+    }
+  }
+
+  Future<void> _importDatabase() async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['zip'],
+      );
+      if (result == null) return;
+
+      File selectedFile = File(result.files.single.path!);
+      final appDir = await getApplicationDocumentsDirectory();
+      final databasePath = await getDatabasesPath();
+
+      final zipBytes = await selectedFile.readAsBytes();
+      final archive = ZipDecoder().decodeBytes(zipBytes);
+
+      for (final file in archive) {
+        final filePath = file.name == 'wispar.db'
+            ? '$databasePath/${file.name}'
+            : '${appDir.path}/${file.name}';
+        final outFile = File(filePath);
+        await outFile.create(recursive: true);
+        await outFile.writeAsBytes(file.content as List<int>);
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(AppLocalizations.of(context)!.databaseImported)),
+      );
+
+      await openDatabase('$databasePath/wispar.db');
+    } catch (e) {
+      debugPrint("Database import error: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(AppLocalizations.of(context)!.databaseImportFailed)),
       );
     }
   }
@@ -55,81 +148,99 @@ class _DatabaseSettingsScreenState extends State<DatabaseSettingsScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                controller: _cleanupIntervalController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: AppLocalizations.of(context)!.cleanupInterval,
-                  hintText: AppLocalizations.of(context)!.cleanupIntervalHint,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: _cleanupIntervalController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(context)!.cleanupInterval,
+                    hintText: AppLocalizations.of(context)!.cleanupIntervalHint,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      _cleanupInterval =
+                          int.tryParse(value) ?? _cleanupInterval;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return AppLocalizations.of(context)!
+                          .cleanupIntervalInvalidNumber;
+                    }
+                    final intValue = int.tryParse(value);
+                    if (intValue == null || intValue < 1 || intValue > 365) {
+                      return AppLocalizations.of(context)!
+                          .cleanupIntervalNumberNotBetween;
+                    }
+                    return null;
+                  },
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    _cleanupInterval = int.tryParse(value) ?? _cleanupInterval;
-                  });
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return AppLocalizations.of(context)!
-                        .cleanupIntervalInvalidNumber;
-                  }
-                  final intValue = int.tryParse(value);
-                  if (intValue == null || intValue < 1 || intValue > 365) {
-                    return AppLocalizations.of(context)!
-                        .cleanupIntervalNumberNotBetween;
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              DropdownButtonFormField<int>(
-                value: _fetchInterval,
-                onChanged: (int? newValue) {
-                  setState(() {
-                    _fetchInterval = newValue!;
-                  });
-                },
-                decoration: InputDecoration(
-                  labelText: AppLocalizations.of(context)!.apiFetchInterval,
-                  hintText: AppLocalizations.of(context)!.apiFetchIntervalHint,
+                const SizedBox(height: 16),
+                DropdownButtonFormField<int>(
+                  value: _fetchInterval,
+                  onChanged: (int? newValue) {
+                    setState(() {
+                      _fetchInterval = newValue!;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: AppLocalizations.of(context)!.apiFetchInterval,
+                    hintText:
+                        AppLocalizations.of(context)!.apiFetchIntervalHint,
+                  ),
+                  items: [
+                    DropdownMenuItem(
+                      value: 3,
+                      child: Text('3 ${AppLocalizations.of(context)!.hours}'),
+                    ),
+                    DropdownMenuItem(
+                      value: 6,
+                      child: Text('6 ${AppLocalizations.of(context)!.hours}'),
+                    ),
+                    DropdownMenuItem(
+                      value: 12,
+                      child: Text('12 ${AppLocalizations.of(context)!.hours}'),
+                    ),
+                    DropdownMenuItem(
+                      value: 24,
+                      child: Text('24 ${AppLocalizations.of(context)!.hours}'),
+                    ),
+                    DropdownMenuItem(
+                      value: 48,
+                      child: Text('48 ${AppLocalizations.of(context)!.hours}'),
+                    ),
+                    DropdownMenuItem(
+                      value: 72,
+                      child: Text('72 ${AppLocalizations.of(context)!.hours}'),
+                    ),
+                  ],
                 ),
-                items: [
-                  DropdownMenuItem(
-                    value: 3,
-                    child: Text('3 ${AppLocalizations.of(context)!.hours}'),
-                  ),
-                  DropdownMenuItem(
-                    value: 6,
-                    child: Text('6 ${AppLocalizations.of(context)!.hours}'),
-                  ),
-                  DropdownMenuItem(
-                    value: 12,
-                    child: Text('12 ${AppLocalizations.of(context)!.hours}'),
-                  ),
-                  DropdownMenuItem(
-                    value: 24,
-                    child: Text('24 ${AppLocalizations.of(context)!.hours}'),
-                  ),
-                  DropdownMenuItem(
-                    value: 48,
-                    child: Text('48 ${AppLocalizations.of(context)!.hours}'),
-                  ),
-                  DropdownMenuItem(
-                    value: 72,
-                    child: Text('72 ${AppLocalizations.of(context)!.hours}'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _saveSettings,
-                child: Text(AppLocalizations.of(context)!.saveSettings),
-              ),
-            ],
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _saveSettings,
+                  child: Text(AppLocalizations.of(context)!.saveSettings),
+                ),
+                const SizedBox(height: 64),
+                const Divider(),
+                const SizedBox(height: 8),
+                ElevatedButton.icon(
+                  onPressed: _exportDatabase,
+                  icon: Icon(Icons.save_alt),
+                  label: Text(AppLocalizations.of(context)!.exportDatabase),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton.icon(
+                  onPressed: _importDatabase,
+                  icon: Icon(Icons.upload_file_outlined),
+                  label: Text(AppLocalizations.of(context)!.importDatabase),
+                ),
+              ],
+            ),
           ),
         ),
       ),
