8fb8bd8193d37e2a0d08f0178ea69ffc0ba5400b
Francis Lapointe <98601298+Scriptbash@users.noreply.github.com>
2025-04-12 11:41:03 -0400

    Replace existing issns (#115)
    * Replace existing issns
    
    * Remove redundant update

--- lib/services/database_helper.dart
+++ lib/services/database_helper.dart
@@ -185,20 +185,28 @@ class DatabaseHelper {
         );
 
         for (final issn in journal.issn) {
-          await db.insert('journal_issns', {
-            'issn': issn,
-            'journal_id': journalId,
-          });
+          await db.insert(
+            'journal_issns',
+            {
+              'issn': issn,
+              'journal_id': journalId,
+            },
+            conflictAlgorithm: ConflictAlgorithm.replace,
+          );
         }
       }
     } else {
       final journalId = await db.insert('journals', journal.toMap());
 
       for (final issn in journal.issn) {
-        await db.insert('journal_issns', {
-          'issn': issn,
-          'journal_id': journalId,
-        });
+        await db.insert(
+          'journal_issns',
+          {
+            'issn': issn,
+            'journal_id': journalId,
+          },
+          conflictAlgorithm: ConflictAlgorithm.replace,
+        );
       }
     }
   }
