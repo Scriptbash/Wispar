c0e4f4959ba247a63add4372a9fc0d3767ce9a2b
Scriptbash <98601298+Scriptbash@users.noreply.github.com>
2025-04-18 16:53:36 -0400

    Add follow button to journal info screen

--- lib/widgets/journal_header.dart
+++ lib/widgets/journal_header.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import '../generated_l10n/app_localizations.dart';
+import '../models/crossref_journals_models.dart' as Journals;
+import './journal_follow_button.dart';
 
 class JournalInfoHeader extends SliverPersistentHeaderDelegate {
+  final String title;
   final String publisher;
   final String issn;
+  final bool isFollowed;
+  final Function(bool) onFollowStatusChanged;
 
   JournalInfoHeader({
+    required this.title,
     required this.publisher,
     required this.issn,
+    required this.isFollowed,
+    required this.onFollowStatusChanged,
   });
 
   @override
@@ -19,21 +27,57 @@ class JournalInfoHeader extends SliverPersistentHeaderDelegate {
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
+    // a little bit hacky, but oh well :(
+    final journalItem = Journals.Item(
+      lastStatusCheckTime: 0,
+      counts: Journals.Counts(currentDois: 0, backfileDois: 0, totalDois: 0),
+      breakdowns: Journals.Breakdowns(doisByIssuedYear: [
+        [0]
+      ]),
+      publisher: publisher,
+      coverage: {},
+      title: title,
+      coverageType: Journals.CoverageType(all: {}, backfile: {}, current: {}),
+      flags: {},
+      issn: issn.split(','),
+      issnType: [],
+    );
+
     return SingleChildScrollView(
       child: Container(
-        padding: EdgeInsets.only(left: 15.0),
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 15.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 8.0),
-            Text('${AppLocalizations.of(context)!.publisher}: $publisher'),
-            Text('ISSN: $issn'),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 8.0),
+                      Text(
+                          '${AppLocalizations.of(context)!.publisher}: ${publisher}'),
+                      Text('ISSN: ${issn}'),
+                      SizedBox(height: 8.0),
+                    ],
+                  ),
+                ),
+                title.isNotEmpty && publisher.isNotEmpty && issn.isNotEmpty
+                    ? FollowButton(
+                        item: journalItem,
+                        isFollowed: isFollowed,
+                        onFollowStatusChanged: onFollowStatusChanged,
+                        buttonType: ButtonType.outlined,
+                      )
+                    : SizedBox.shrink(),
+              ],
+            ),
             SizedBox(height: 8.0),
             Container(
               height: 1,
-              margin: EdgeInsets.symmetric(
-                horizontal: 60,
-              ),
+              margin: EdgeInsets.symmetric(horizontal: 60),
               color: Colors.grey,
             ),
           ],
@@ -43,7 +87,9 @@ class JournalInfoHeader extends SliverPersistentHeaderDelegate {
   }
 
   @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return false;
+  bool shouldRebuild(covariant JournalInfoHeader oldDelegate) {
+    return oldDelegate.isFollowed != isFollowed ||
+        oldDelegate.issn != issn ||
+        oldDelegate.publisher != publisher;
   }
 }
