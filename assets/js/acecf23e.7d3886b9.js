4a62c1fe922906d830633f940e7c5b0631cd4588
Scriptbash <Scriptbash@users.noreply.github.com>
2026-03-30 14:10:47 +0000

    deploy: 10820bd8c4b79af91ce4c30f8d50c205749d34e2

--- assets/js/acecf23e.7d3886b9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkwispar=globalThis.webpackChunkwispar||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
